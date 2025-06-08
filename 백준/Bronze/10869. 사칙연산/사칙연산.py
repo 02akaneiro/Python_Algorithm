7a270a7ccdea0249d3c5c734630b6513c561b067
02akaneiro <gpdmsabc18@gmail.com>
2025-06-09 02:17:00 +0900

    [Bronze V] Title: 사칙연산, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,13 @@
+result = 0
+a,b = list(map(int,input().split()))
+
+result = a + b
+print(result)
+result = a - b
+print(result)
+result = a * b
+print(result)
+result = a / b
+print(int(result))
+result = a % b
+print(result)
